031e0ee618f97fe8859ce8df03b2db0e36864725
Herul Syahwandi S <34206362+galontong@users.noreply.github.com>
2022-02-25 10:22:49 +0700

    Update Program.cs

--- BasicGit/BasicGit/Program.cs
+++ BasicGit/BasicGit/Program.cs
@@ -11,5 +11,40 @@ namespace BasicGit
             Console.WriteLine("From Herul Syahwandi versi 2");
             Console.WriteLine(" Reo Sahobby");
         }
+
+        static void Herul()
+        {
+            Console.WriteLine("From Herul Syahwandi versi 2");
+        }
+
+        static void Achmad()
+        {
+
+        }
+
+        static void Denny()
+        {
+
+        }
+
+        static void Rizky()
+        {
+
+        }
+        
+        static void Reo()
+        {
+
+        }
+
+        static void Rezky()
+        {
+
+        }
+
+        static void Hanif()
+        {
+
+        }
     }
 }
